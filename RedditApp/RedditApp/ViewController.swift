9a8d617b6626ee4ad2cf47927e3c217860af7a71
Marco Siracusano <marc.siracusano@gmail.com>
2020-10-10 12:26:45 -0300

    Creating project

---
+++ RedditApp/RedditApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RedditApp
+//
+//  Created by Marco Siracusano on 10/10/2020.
+//  Copyright © 2020 Marco Siracusano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
