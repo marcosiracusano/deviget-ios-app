7cf9a20ac615043e07879b4b96211a952133f1f5
Marco Siracusano <marc.siracusano@gmail.com>
2020-10-10 23:04:49 -0300

    Save image in Photo Gallery behavior setup.

--- RedditApp/RedditApp/ViewController/DetailViewController.swift
+++ RedditApp/RedditApp/ViewController/DetailViewController.swift
@@ -15,10 +15,10 @@ class DetailViewController: UIViewController {
     @IBOutlet weak var postContentLabel: UILabel!
     
     var post: Post?
+    var hasImage: Bool = true
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
         setupPostContent()
     }
 
@@ -30,9 +30,26 @@ class DetailViewController: UIViewController {
         if let post = post, let url = URL(string: post.thumbnail), UIApplication.shared.canOpenURL(url) {
             thumbnailImageView.load(url: url)
         } else {
+            hasImage = false
             thumbnailImageView.image = UIImage(named: "imagePlaceholder")
             thumbnailImageView.image = thumbnailImageView.image?.withRenderingMode(.alwaysTemplate)
             thumbnailImageView.tintColor = UIColor(red: 235/255, green: 85/255, blue: 40/255, alpha: 1)
+        }
+    }
+    
+    @IBAction func tap(_ sender: Any) {
+        if let image = thumbnailImageView.image, hasImage {
+            UIImageWriteToSavedPhotosAlbum(image, self, #selector(saveError), nil)
+        }
+    }
+    
+    @objc func saveError(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
+        let alert = UIAlertController(title: nil, message: "Image saved in Photo Library", preferredStyle: .alert)
+
+        present(alert, animated: true)
+        
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 2) {
+            alert.dismiss(animated: true)
         }
     }
 }
